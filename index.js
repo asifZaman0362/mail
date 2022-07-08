9926f297bbfc728105823714b6f06666f2f246d6
zero362001 <zero362001@protonmail.com>
2022-07-08 22:07:15 +0530

    updated code

--- index.js
+++ index.js
@@ -109,13 +109,13 @@ app.get('*', (req, res) => {
 https
     .createServer(
         {
-        key: fs.readFileSync("server.key"),
-        cert: fs.readFileSync("server.cert"),
+            key: fs.readFileSync("server.key"),
+            cert: fs.readFileSync("server.cert"),
         },
         app
     )
     .listen(3000, function () {
-        console.log(
-        "Example app listening on port 3000! Go to https://localhost:3000/"
+            console.log(
+            "Example app listening on port 3000! Go to https://localhost:3000/"
         );
     });
